1c9b4565dc9daabfbab34d64a0a6fb87305bb75e
Anna Daniuk <danyukanya57@yandex.ru>
2025-11-16 22:15:55 +0300

    Добавляет блок form, стилия для mobile и стили состояния

--- source/js/main.js
+++ source/js/main.js
@@ -1,5 +1,7 @@
 import { initMobileMenu } from './header/menu.js';
 import { initHeroSlider } from './hero/hero-slider.js';
+import { validateForm } from './form/form-validation.js';
 
 initMobileMenu();
 initHeroSlider();
+validateForm();
